ffca56c6ca3f65a76c88629ffc8bef39c53253ae
Nicole Cruz <nickruz7301@hotmail.com>
2024-04-22 19:02:04 -0300

    sorteador para 50

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 //paragrafo.innerHTML = 'Escolha um número de 1 a 100';
 
 let listasDeNumerosSorteados = [];
-let numeroLimite = 100
+let numeroLimite = 50
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
 exibirTextoNaTela('h1', 'jogo do número secreto');
-exibirTextoNaTela('p', "Escolha um número de 1 a 100");
+exibirTextoNaTela('p', "Escolha um número de 1 a 50");
 }
 
 exibirMensagemInicial()
